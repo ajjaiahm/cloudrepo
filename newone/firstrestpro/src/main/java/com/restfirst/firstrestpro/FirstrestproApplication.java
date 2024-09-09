009193fae5f3e7cd00d62886f74890ee6cd8cc98
Hari-SEPP <167086044+Hari-SEPP@users.noreply.github.com>
2024-09-09 10:32:44 +0530

    Add files via upload

---
+++ newone/firstrestpro/src/main/java/com/restfirst/firstrestpro/FirstrestproApplication.java
@@ -0,0 +1,13 @@
+package com.restfirst.firstrestpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstrestproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstrestproApplication.class, args);
+	}
+
+}
---
+++ newone/firstrestpro/src/test/java/com/restfirst/firstrestpro/FirstrestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfirst.firstrestpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrestproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
